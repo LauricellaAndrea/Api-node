79952d7e9b0e673bf767488eac39e548bb166e95
LauricellaAndrea <andrealauricella97@gmail.com>
2023-01-20 17:31:28 +0100

    Add validation exercise

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from "express"
+import {PrismaClient} from "@prisma/client"
+
+import { 
+    validate,
+    validationErrorMiddleware,
+    planetSchema,
+    PlanetData
+} from "./lib/validation"
+
+const app = express()
+
+const prisma = new PrismaClient()
+
+app.use(express.json())
+
+app.get("/planets", async (request, response) => {
+    const planets = await prisma.planet.findMany()
+    response.json(planets)
+})
+app.post("/planets", validate({body: planetSchema}),  async (request, response) => {
+    const planetData: PlanetData = request.body;
+
+    const planet = await prisma.planet.create({
+        data: planetData
+    })
+
+    response.json(planet)
+})
+
+app.use(validationErrorMiddleware)
+
+app.listen(3000, ()=> console.log("running on port",3000 ))

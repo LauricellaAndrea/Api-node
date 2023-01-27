fbefbdb44aee88e066e9b5e3158f6bb014f5609b
LauricellaAndrea <andrealauricella97@gmail.com>
2023-01-27 18:00:48 +0100

    add-unique-id

--- src/app.ts
+++ src/app.ts
@@ -12,12 +12,19 @@ const app = express()
 const prisma = new PrismaClient()
 app.use(express.json())
 
-//get
+//Get Unique ID
 
-app.get("/planets", async (request, response) => {
-    const planets = await prisma.planet.findMany()
-    response.json(planets)
+app.get("/planets/:id", async (request, response) => {
+    const planetId = Number(request.params.id)
+
+    const planet = await prisma.planet.findUnique({
+        where: { id: planetId } 
+    });
+
+    response.json(planet)
 })
+
+//Post method
 app.post("/planets", validate({body: planetSchema}),  async (request, response) => {
     const planetData: PlanetData = request.body;
 

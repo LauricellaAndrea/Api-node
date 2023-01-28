6d61256c6b159e01b87cc7b5fcae3edeebe40546
LauricellaAndrea <andrealauricella97@gmail.com>
2023-01-28 19:33:53 +0100

    add validation and route

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,13 @@ const app = express()
 const prisma = new PrismaClient()
 app.use(express.json())
 
+// GET MANY 
+
+app.get("/planets", async (request, response) => {
+    const planets = await prisma.planet.findMany()
+    response.json(planets)
+})
+
 //Get Unique ID
 
 app.get("/planets/:id", async (request, response) => {
@@ -36,5 +43,35 @@ app.post("/planets", validate({body: planetSchema}),  async (request, response) 
 })
 
 app.use(validationErrorMiddleware)
+
+// Put method
+
+app.put("/planets/:id", async (request, response)=>{
+    const {id} = request.params
+    const { name, diameter, moons} = request.body
+    const planet = await prisma.planet.update({
+        where: {
+            id: Number(id)
+        },
+        data: {
+            name,
+            diameter,
+            moons
+        }
+    })
+    response.json(planet)
+})
+
+// Delete method
+
+app.delete("/planets/:id", async (request, response) => {
+    const {id} = request.params
+    const planet = await prisma.planet.delete({
+        where: {
+            id: Number(id)
+        }
+    })
+    response.json(planet)
+})
 
 app.listen(3000, ()=> console.log("running on port",3000 ))

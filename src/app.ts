daf3f60d7e57600561df1ab15a4e4a885d668ab3
LauricellaAndrea <andrealauricella97@gmail.com>
2023-01-28 19:53:02 +0100

    Add support for cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express"
 import {PrismaClient} from "@prisma/client"
+import cors from "cors";
+
 
 import { 
     validate,
@@ -8,9 +10,18 @@ import {
     PlanetData
 } from "./lib/validation"
 
+const corsOption = {
+    origin: "http://localhost:8080"
+}
+
+
 const app = express()
+
 const prisma = new PrismaClient()
-app.use(express.json())
+
+app.use(express.json());
+
+app.use(cors(corsOption)); //cors middleware
 
 // GET MANY 
 
